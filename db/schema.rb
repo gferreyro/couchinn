3483ce8cd5559f2a082cb2cda6278d9593b24130
guidobitonte <guido_b88@hotmail.com>
2016-10-31 17:42:34 -0300

    premium
    si se rompió algo, no fui yo. No está terminado, falta el validar

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 20161012212510) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
-    t.integer  "Edad",                   default: 0,     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
