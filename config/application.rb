50b004f6f562c1c9b43e3d4bda42faf77af1fac0
Gustavo S Ferreyro <gustavo.ferreyro@ucalpvirtual.edu.ar>
2016-10-31 23:30:43 -0300

    Heroku: assets

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, :assets, Rails.env)
 
 module Couchinn
   class Application < Rails::Application
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
